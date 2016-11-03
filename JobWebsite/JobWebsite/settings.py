18aed855c7b79517e5a02e7bebfe7b633a247f74
MarcusStockton <marcus_stockton@hotmail.co.uk>
2016-11-03 20:32:35 +0000

    2nd commit - added a jobs app and set up url mappings

--- JobWebsite/JobWebsite/settings.py
+++ JobWebsite/JobWebsite/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # website modules
+    'jobs',
+
+    # 3rd party apps:
+
 ]
 
 MIDDLEWARE_CLASSES = [
