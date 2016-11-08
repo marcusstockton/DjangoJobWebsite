a9823dd246bce6d2faef3247d70349b04611e73c
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2016-11-08 08:34:42 +0000

    Extending user model, database drop required.

--- JobWebsite/Attachment/models.py
+++ JobWebsite/Attachment/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Attachment(models.Model):
@@ -8,7 +8,7 @@ class Attachment(models.Model):
 	created_date = models.DateField(auto_now_add=True)
 	file_type = models.CharField(max_length=120)
 	active = models.BooleanField()
-	User = models.ForeignKey(User, on_delete=models.CASCADE)
+	User = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.title
--- JobWebsite/User/models.py
+++ JobWebsite/User/models.py
@@ -1,14 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
+
 
 from Attachment import models as attachment
 
-class User(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+class User(AbstractUser):
 	birth_date = models.DateField(null=True, blank=True)
-	attachments = models.ForeignKey(attachment.Attachment, on_delete=models.CASCADE)
-
+	
 	def __str__(self):
 		return self.title
 
