134321c6134c9c6fe37cad065ab651710123188b
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2016-11-08 09:29:22 +0000

    Fixing user view

--- JobWebsite/User/models.py
+++ JobWebsite/User/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
 	birth_date = models.DateField(null=True, blank=True)
 	
 	def __str__(self):
-		return self.title
+		return self.username
 
 	def get_absolute_url(self):
 		return reverse("users:detail", kwargs={"pk": self.id})
--- JobWebsite/User/views.py
+++ JobWebsite/User/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.models import User
+from .models import User
 from django.urls import reverse_lazy
 
 
