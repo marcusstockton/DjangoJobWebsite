64e7b2d96282c23dfd6f53a0a1626a6fc6c0abc4
MarcusStockton <marcus_stockton@hotmail.co.uk>
2016-12-01 07:55:28 +0000

    Given up on saving images to db Setting the image upload location to username / filename

--- JobWebsite/User/models.py
+++ JobWebsite/User/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
 
+
 def upload_location(instance, filename):
-	return 'user_{0}/{1}'.format(instance.user.id, filename)
+    return '{0}/{1}'.format(instance.username, filename)
 
 
 class User(AbstractUser):
-	birth_date = models.DateField(null=True, blank=True)
-	avatar = models.ImageField(upload_to=upload_location, null=True, blank=True)
-	cv = models.FileField(upload_to=upload_location, blank=True)
-	
-	def __str__(self):
-		return self.username
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(upload_to=upload_location, null=True, blank=True)
+    cv = models.FileField(upload_to=upload_location, blank=True)
 
-	def get_absolute_url(self):
-		return reverse("users:detail", kwargs={"pk": self.id})
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        return reverse("users:detail", kwargs={"pk": self.id})
