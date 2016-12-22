630ebca54d0b74ae0d71253a5a41747ec9076b88
MarcusStockton <marcus_stockton@hotmail.co.uk>
2016-12-22 19:45:39 +0000

    Tidying up issues. Creating new data

--- JobWebsite/Company/views.py
+++ JobWebsite/Company/views.py
@@ -8,66 +8,66 @@ from .models import Company
 
 
 def company_create(request):
-	form = CompanyForm(request.POST or None, request.FILES or None)
-	if form.is_valid():
-		instance = form.save(commit=False)
-		instance.user = request.user
-		instance.save()
-		messages.success(request, "Sucessfully Created")
-		return HttpResponseRedirect(instance.get_absolute_url())
-	context = {
-		"form": form,
-	} # TODO Create create.html file  in company templates
-	return render(request, "company/create.html", context)
+    form = CompanyForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
+        messages.success(request, "Sucessfully Created")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "form": form,
+    } # TODO Create create.html file  in company templates
+    return render(request, "company/create.html", context)
 
 
 def company_detail(request, pk=None):
-	instance = get_object_or_404(Company, pk=pk)
-	context = {
-		"title": instance.company_name,
-		"instance": instance
-	}
-	return render(request, "company/detail.html", context)
+    instance = get_object_or_404(Company, pk=pk)
+    context = {
+        "title": instance.company_name,
+        "instance": instance
+    }
+    return render(request, "company/detail.html", context)
 
 
 def company_list(request):
-	queryset_list = Company.objects.all()
+    queryset_list = Company.objects.all()
 
-	context = {
-		"title": "List",
-		"object_list": queryset_list
-	 }
-	return render(request, "company/index.html", context)
+    context = {
+        "title": "List",
+        "object_list": queryset_list
+     }
+    return render(request, "company/index.html", context)
 
 
 def company_edit(request, pk=None):	
-	instance = get_object_or_404(Company, pk=pk)
-	form = CompanyEditForm(request.POST or None, instance = instance)
-	# Debugging info
-	if request.POST:
-		for key, value in request.POST.items():
-			print(key + ": " + value)
-		print(form)
-	if form.is_valid():
-		# need a way to update the address type...
-		# instance = form.save(commit=False)
-		# instance.save()
-		# messages.success(request, "Sucessfully Updated")
+    instance = get_object_or_404(Company, pk=pk)
+    form = CompanyEditForm(request.POST or None, instance = instance)
+    # Debugging info
+    if request.POST:
+        for key, value in request.POST.items():
+            print(key + ": " + value)
+        print(form)
+    if form.is_valid():
+        # need a way to update the address type...
+        # instance = form.save(commit=False)
+        # instance.save()
+        # messages.success(request, "Sucessfully Updated")
 
-		return HttpResponseRedirect(instance.get_absolute_url())
+        return HttpResponseRedirect(instance.get_absolute_url())
 
-	context = {
-		"title": instance.company_name,
-		"instance": instance,
-		"form": form,
-	}
-	return render(request, "company/edit.html", context)
+    context = {
+        "title": instance.company_name,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "company/edit.html", context)
 
 
 def company_delete(request, pk=None):
-	instance = get_object_or_404(Company, pk=pk)
-	instance.delete()
-	messages.success(request, "Sucessfully Deleted")
-	return redirect("company:list")
+    instance = get_object_or_404(Company, pk=pk)
+    instance.delete()
+    messages.success(request, "Sucessfully Deleted")
+    return redirect("company:list")
 
-	return HttpResponse("<h1>Delete</h1>")
+    return HttpResponse("<h1>Delete</h1>")
