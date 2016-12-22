f08dec5ad1d91031fd6bc531c085cd741dd8c865
MarcusStockton <marcus_stockton@hotmail.co.uk>
2016-12-22 19:10:20 +0000

    Tidying up issues. Creating new data

--- JobWebsite/Readme.txt
+++ JobWebsite/Readme.txt
@@ -18,9 +18,5 @@ Fix up the format the date is passed in for date of birth in User edit form
 
 The following is how to create related objects. This has been tested in python shell
 
-from Attachment.models import Attachment
-from User.models import User
-nathan = User.objects.create(username='nathan', first_name="nathan", last_name="sykes", email="nat@han.com", password="1234")
-nathan.attachment_set.create(file_name="test.txt", data=bytearray(10), created_date="13/12/2016", file_type="txt", active=True)
-
-Need to implement this in the code, reading out the inmemoryuploadedfile to disk...somehow.
+Create a way to add new JObs / Companies etc
+Change the Nav to show the user name once logged in
--- JobWebsite/User/models.py
+++ JobWebsite/User/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
 
 
-# def upload_location(instance, filename):
-#     return '{0}/{1}'.format(instance.username, filename)
+def upload_location(instance, filename):
+    return '{0}/{1}'.format(instance.username, filename)
 
 
 class User(AbstractUser):
     birth_date = models.DateField(null=True, blank=True, auto_now_add=False)
-    # avatar = models.ImageField(upload_to=upload_location, null=True, blank=True)
-    # cv = models.FileField(upload_to=upload_location, blank=True)
+    avatar = models.ImageField(upload_to=upload_location, null=True, blank=True)
+    cv = models.FileField(upload_to=upload_location, blank=True)
 
     def __str__(self):
         return self.username
