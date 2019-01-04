ceb0b995406871b18f2a828e98c57291639700ef
Marcus Stockton <marcus.stockton@thomsonreuters.com>
2019-01-04 16:22:33 +0000

    refactored test code

--- JobWebsite/Address/tests.py
+++ JobWebsite/Address/tests.py
@@ -15,7 +15,7 @@ class AddressTestCase(TestCase):
         user = User.objects.create_user(
             username="mstockton", email='mstockton@test.com', password='12345')
         Address.objects.create(address_type=AddressType.objects.create(description="Business", is_active=True), address_line_1='Address Line 1',
-                                address_line_2="Address Line 2", post_code="EX11EX", county="Devon", country="United Kingdom")
+                               address_line_2="Address Line 2", post_code="EX11EX", county="Devon", country="United Kingdom")
 
     def test_address_loads_correctly(self):
         """ Tests that addresses are correctly returned from the DB """
@@ -33,43 +33,42 @@ class AddressTestCase(TestCase):
         address.address_type = None
         self.assertRaises(ValidationError, lambda: address.full_clean())
 
+
 class AddressFormTestCase(TestCase):
     """ Test the address form """
     @override_settings(DEBUG=True)
     def setUp(self):
         user = User.objects.create_user(
-            username="mstockton", 
-            email='mstockton@test.com', 
+            username="mstockton",
+            email='mstockton@test.com',
             password='12345')
         Address.objects.create(
             address_type=AddressType.objects.create(
-                description="Business", 
-                is_active=True), 
+                description="Business",
+                is_active=True),
             address_line_1='Address Line 1',
-            post_code="EX11EX", 
-            county="Devon", 
+            post_code="EX11EX",
+            county="Devon",
             country="GB"
-            )
+        )
 
     def test_address_form_init(self):
         form = AddressForm()
         self.assertFalse(form.is_valid())
 
     def test_cleaned_data(self):
-        address_type = AddressType.objects.get(description='Business')
-        address = Address.objects.get(post_code="EX11EX")
-        self.assertIsNotNone(address_type)
+        address = Address.objects.get()  # only one item, so get it
         self.assertIsNotNone(address)
         form = AddressForm(data={
-            'address_type': address_type.id, 
-            'address_line_1': address.address_line_1, 
-            'post_code':address.post_code, 
-            'county': address.county, 
+            'address_type': address.address_type.id,
+            'address_line_1': address.address_line_1,
+            'post_code': address.post_code,
+            'county': address.county,
             'country': address.country})
-            
+
         self.assertTrue(form.is_bound)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.clean_address_line_1(),'Address Line 1')
-        self.assertEqual(form.clean_post_code(),'EX11EX')
-        self.assertEqual(form.clean_county(),'Devon')
-        self.assertEqual(form.clean_country(),'GB')
+        self.assertEqual(form.clean_address_line_1(), 'Address Line 1')
+        self.assertEqual(form.clean_post_code(), 'EX11EX')
+        self.assertEqual(form.clean_county(), 'Devon')
+        self.assertEqual(form.clean_country(), 'GB')
