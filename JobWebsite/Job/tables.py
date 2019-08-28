32b84d863eaa5d90cd19305b7b4f42dcfc7e36cd
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2019-08-28 13:09:42 +0100

    Styling forms

--- JobWebsite/Job/tables.py
+++ JobWebsite/Job/tables.py
@@ -3,11 +3,21 @@ from django_tables2.utils import A
 from .models import Job, JobApplication
 import datetime
 
+
+
+class TruncatedTextColumn(tables.Column):
+    '''A Column to limit to 100 characters and add an ellipsis'''
+    def render(self, value):
+        if len(value) > 102:
+            return value[0:99] + '...'
+        return str(value)
+
+
 class JobTable(tables.Table):
     view = tables.LinkColumn('jobs:detail', text='View', args=[A('pk')], orderable=False)
     edit = tables.LinkColumn('jobs:edit', text='Edit', args=[A('pk')], orderable=False)
     apply = tables.LinkColumn('jobs:apply', text='Apply', args=[A('pk')], orderable=False)
-
+    content = TruncatedTextColumn(accessor=A('content'))
     class Meta:
         model = Job
         exclude = ['id']
@@ -23,4 +33,5 @@ class JobTable(tables.Table):
         else:
             self.columns.show('apply')
             self.columns.hide('edit')
-    
+        
+
